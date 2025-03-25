12129bce73ca8e840856acc1482e0def78a5bb71
Srijan Pusala <srijanpusala@gmail.com>
2025-03-25 20:46:14 +0530

    3rd commit

--- form.js
+++ form.js
@@ -1,50 +1,35 @@
 document.getElementById('inquiry-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    
+  
     const fullName = document.getElementById('full_name').value;
     const phoneNumber = document.getElementById('phone_number').value;
     const email = document.getElementById('email').value;
     const query = document.getElementById('query').value;
-    
-    const formData = {
-        fullName,
-        phoneNumber,
-        email,
-        query
-    };
-    
-    console.log('Submitting form data:', formData);
-    
-    fetch('https://script.google.com/macros/s/AKfycbwrxPzew25M-5bnMeSf8l0TLN-xpogxl8PX8kp1PmBf0Du2qWCwiS2H82ToBjc8HknPig/exec', {
-        method: 'POST',
-        body: JSON.stringify(formData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+  
+    const formData = { fullName, phoneNumber, email, query };
+  
+    fetch('https://script.google.com/macros/s/AKfycby-vqLs4CrBWK1J6QrBtUlLCe-UWGpqLTFugJKJ5yWMxUPBYfmfAptB_GxbLks0eSbYgA/exec', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     })
     .then(response => {
-        console.log('Response:', response);
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
+      if (!response.ok) throw new Error('Network response was not ok');
+      return response.json();
     })
     .then(data => {
-        console.log('Response data:', data);
-        if (data.result === 'success') {
-            const popup = document.getElementById('thank-you-popup');
-            popup.classList.remove('hidden');
-            
-            setTimeout(() => {
-                window.location.href = 'index.html';
-            }, 5000);
-        } else {
-            console.error('Error in response data:', data);
-            alert('There was an error submitting your query. Please try again.');
-        }
+      if (data.result === 'success') {
+        const popup = document.getElementById('thank-you-popup');
+        popup.classList.remove('hidden');
+        setTimeout(() => { window.location.href = 'index.html'; }, 5000);
+      } else {
+        alert('Error submitting query. Please try again.');
+      }
     })
     .catch(error => {
-        console.error('Fetch error:', error);
-        alert('There was an error submitting your query. Please try again.');
+      console.error('Fetch error:', error);
+      alert('Error submitting query. Please try again.');
     });
-});
+  });
